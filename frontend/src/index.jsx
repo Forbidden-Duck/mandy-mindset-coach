a3e312735ec40ce38fe9b1c59a3c5076a5628d2e
Harrison Howard <harrison.howard00707@gmail.com>
2022-03-14 14:23:21 +1000

    Add empty store and reducers

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,22 +5,23 @@ import "./index.css";
 
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-// TODO import reducer from store
+import reducer from "./store";
 
-import { ThemeProvider, createTheme } from "@mui/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 // Message for those who are looking at the console
 console.log("Welcome to the console!");
 
-// TODO configure store
+const store = configureStore({ reducer });
 const theme = createTheme({});
 
 ReactDOM.render(
     <StrictMode>
-        {/* TODO add store to provider */}
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </Provider>
     </StrictMode>,
     document.getElementById("root")
 );
