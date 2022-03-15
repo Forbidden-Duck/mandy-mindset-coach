0c2ecb8fe590b636da8ab53e7ae77b4945d5efbc
Harrison Howard <harrison.howard00707@gmail.com>
2022-03-15 15:49:49 +1000

    Import project resources and preload them so they are cached

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // TODO setup app route
 
+const PRELOAD_IMAGE_URLS = [
+    "resources/mandy-portrait.jpg",
+    "resources/thryve-full-transparent.png",
+    "resources/thryve-full-white.png",
+    "resources/thryve-logo-transparent.png",
+    "resources/thryve-logo-white.png",
+];
+
 function App() {
+    // Have the browser preload the images
+    useEffect(() => {
+        PRELOAD_IMAGE_URLS.forEach((url) => {
+            new Image().src = url;
+        });
+    }, []);
+
     return (
         <Router>
             <Routes>
