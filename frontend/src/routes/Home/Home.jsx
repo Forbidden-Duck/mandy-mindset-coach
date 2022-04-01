323bd7856fd25b38d844ec6871dc21d5c9b5fcb3
Harrison Howard <harrison.howard00707@gmail.com>
2022-04-01 16:56:51 +1100

    Start of home page

--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { makeStyles } from "@mui/styles";
+import hexToRGB from "../../utils/hexToRGB";
+import { Typography } from "@mui/material";
 
 function Home() {
-    return <h1>Home</h1>;
+    const classes = makeStyles((theme) => ({
+        container: {
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+        },
+        group: {
+            display: "flex",
+        },
+        section: {
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+        },
+        portrait: {
+            backgroundImage: "url(/resources/mandy-portrait.jpg)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            height: "500px",
+            width: "326px",
+            zIndex: -1,
+        },
+        portraitContainer: {
+            display: "flex",
+            background: `rgba(${Object.values(
+                hexToRGB(theme.palette.primary.dark)
+            )}, 0.35)`,
+        },
+        contentContainer: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            background: `rgba(${Object.values(
+                hexToRGB(theme.palette.secondary.dark)
+            )}, 0.3)`,
+            width: "100%",
+        },
+    }))();
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.group}>
+                <div className={classes.portraitContainer}>
+                    <div className={classes.portrait} />
+                </div>
+                <div className={classes.contentContainer}>
+                    <div style={{ maxWidth: "1000px", margin: "0rem 2rem" }}>
+                        <Typography variant="h3" fontFamily="Kaushan Script">
+                            Lorem ipsum dolor sit amet.
+                        </Typography>
+                        <Typography variant="body2" fontSize="1rem">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Dolor eveniet similique totam repellendus id
+                            ut minima rerum voluptatem atque sit?
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
