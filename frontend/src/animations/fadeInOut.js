b21c206e0fac0caea08bb3b7cfe1448a3a2a7981
Harrison Howard <harrison.howard00707@gmail.com>
2022-04-05 13:14:16 +1000

    Allow no exit in the animation

--- frontend/src/animations/fadeInOut.js
+++ frontend/src/animations/fadeInOut.js
@@ -1,10 +1,11 @@
 /**
- * @param {number} duration
- * @param {number} delay
+ * @param {number} duration default: 0.5
+ * @param {number} delay default: 0
+ * @param {boolean} exit default: true
  * @return {import("framer-motion").MotionProps}
  */
-const fadeInOut = (duration = 0.5, delay = 0) => {
-    return {
+const fadeInOut = (duration = 0.5, delay = 0, exit = true) => {
+    const output = {
         initial: {
             opacity: 0,
         },
@@ -19,5 +20,7 @@ const fadeInOut = (duration = 0.5, delay = 0) => {
             delay,
         },
     };
+    if (!exit) delete output.exit;
+    return output;
 };
 export default fadeInOut;
