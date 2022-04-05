94007283e0ccc1d99af043d6365cef44011d3a0b
Harrison Howard <harrison.howard00707@gmail.com>
2022-04-05 15:47:02 +1000

    Change how slides are calculated (allow custom val)

--- frontend/src/animations/slideDown.js
+++ frontend/src/animations/slideDown.js
@@ -1,22 +1,23 @@
 /**
  * @param {boolean} cont default: true
  * @param {boolean} exit default: true
+ * @param {string} customVal default: %
  * @return {import("framer-motion").MotionProps}
  */
-const slideDown = (cont = true, exit = true) => {
+const slideDown = (cont = true, exit = true, customVal = "%") => {
     const output = {
         initial: {
-            y: "-100vh",
+            y: `-100${customVal}`,
         },
         animate: {
             y: 0,
         },
         exit: {
-            y: "100vh",
+            y: `100${customVal}`,
         },
     };
     if (!exit) delete output.exit;
-    if (!cont) output.exit = { y: "-100vh" };
+    if (!cont) output.exit = { y: `-100${customVal}` };
     return output;
 };
 export default slideDown;
--- frontend/src/animations/slideLeft.js
+++ frontend/src/animations/slideLeft.js
@@ -1,25 +1,26 @@
 /**
  * @param {boolean} cont default: true
  * @param {boolean} exit default: true
+ * @param {string} customVal default: %
  * @return {import("framer-motion").MotionProps}
  */
-const slideLeft = (cont = true, exit = true) => {
+const slideLeft = (cont = true, exit = true, customVal = "%") => {
     /**
      * @type {import("framer-motion").MotionProps}
      */
     const output = {
         initial: {
-            x: "100%",
+            x: `100${customVal}`,
         },
         animate: {
             x: 0,
         },
         exit: {
-            x: "-100%",
+            x: `-100${customVal}`,
         },
     };
     if (!exit) delete output.exit;
-    if (!cont) output.exit = { x: "100%" };
+    if (!cont) output.exit = { x: `100${customVal}` };
     return output;
 };
 export default slideLeft;
