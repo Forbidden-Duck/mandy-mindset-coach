1e796c35bb69ffcba1a9ed9051de614d2e49d9b8
Harrison Howard <harrison.howard00707@gmail.com>
2022-06-02 10:36:58 +1000

    Preload images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ const PRELOAD_IMAGE_URLS = [
     "resources/thryve-full-white.png",
     "resources/thryve-logo-transparent.png",
     "resources/thryve-logo-white.png",
+    "https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822__340.jpg",
+    "https://cdn.pixabay.com/photo/2018/03/03/20/02/laptop-3196481__340.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/23/12/37/files-1614223__340.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/29/08/55/work-1627703__340.jpg",
+    "https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333__480.jpg",
 ];
 
 function App() {
